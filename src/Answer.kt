f2f29c44e712cfcdd8c5a5f9ee8e3b5f972f8345
root <1025567884@qq.com>
2019-04-18 10:58:19 +0800

    Initial commit

---
+++ src/Answer.kt
@@ -0,0 +1,15 @@
+/**
+ * 一个思维锻炼的项目
+ * 请坚持下去 每日至少一题
+ * 记录员：Francis
+ * 日期：2019年4月18日
+ **/
+// 227 基本计算器II
+fun calculate(s: String): Int {
+
+    return 0
+}
+
+fun main(args: Array<String>) {
+    calculate("")
+}
