68c0719c0abb1cb74264850064ccec9cac07e454
root <1025567884@qq.com>
2019-04-23 11:28:40 +0800

    完成了 227题

--- src/Answer.kt
+++ src/Answer.kt
@@ -9,54 +9,62 @@ val calculates = arrayOf("+", "-", "*", "/")
 
 fun calculate(s: String): Int {
     //用+号将所有的有加法计算的先隔离出来
-    val str = "$s+"
+    var str = "$s+"
+    while (str.contains(" ")) {
+        str = str.replace(" ", "")
+    }
+
+    println(str.length)
     var index = 0
     var j = 0
     var i: Int
     var last = mutableListOf(0)
-    val array = mutableListOf<Int>()
-    val array2 = mutableListOf<Int>()
+    val array = mutableListOf(0)
     while (index < str.length) {
         i = calculates.indexOf(str[index].toString())
         if (i != -1) {
             val a = str.substring(j, index).toInt()
             j = index + 1
             last.add(i)
-            if (array2.size > 0) {
-                array2.add(a)
-            } else {
-                when (i) {
-                    //加减
-                    0, 1 -> {
-                        array.add(a)
-                    }
-                    2, 3 -> {
-                        array2.add(a)
-                    }
+            array.add(a)
+            val cal = last[last.size - 2]
+
+            if (i > 1 && cal < 2) {
+                println(last.toString())
+                index++
+                continue
+            }
+
+
+            when (array.size) {
+                3 -> {
+                    array[1] = operation(last[last.size - 2], mutableListOf(array[1], array[2]))
+                    array.removeAt(2)
+                    last.removeAt(last.size - 2)
+                }
+                2 -> {
+                    array[0] = operation(last[last.size - 2], array)
+                    array.removeAt(1)
+                    last.removeAt(last.size - 2)
+                }
+            }
+            if (i < 2) {
+                while (array.size > 1) {
+                    array[0] = operation(last[last.size - 2], array)
+                    array.removeAt(1)
+                    last.removeAt(last.size - 2)
                 }
             }
         }
-        if (array2.size > 1) {
-            println(last.toString())
-            array2[0] = operation(last[last.size - 2], array2)
-            last.removeAt(last[last.size - 2])
-            array.add(array2[0])
-            array2.clear()
-        }
-        if (array.size > 1) {
-            println(last.toString())
-            array[0] = operation(last[last.size - 2], array)
-            array.removeAt(1)
-            last.removeAt(last[last.size - 2])
-        }
         index++
     }
-    println()
+    println(array.toString())
+
     return array[0]
 }
 
 fun operation(cal: Int, array: MutableList<Int>): Int {
-    print("${array[0]}-->$cal---->${array[1]}=")
+    print("${array[0]}${calculates[cal]}${array[1]}=")
     when (cal) {
         0 -> array[0] += array[1]
         1 -> array[0] -= array[1]
@@ -69,6 +77,6 @@ fun operation(cal: Int, array: MutableList<Int>): Int {
 
 
 fun main(args: Array<String>) {
-    println(calculate("30+20*20-30+50/20"))
+    println(calculate("1+2*5/3+6/4*2"))
 
 }
