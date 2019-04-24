8ef2d75591a0725de8ff1b5e910096d50c43342b
root <1025567884@qq.com>
2019-04-24 10:59:03 +0800

    增加题目 665

---
+++ src/665/Answer.kt
@@ -0,0 +1,17 @@
+//package `665`
+
+/**
+ * 一个思维锻炼的项目
+ * 请坚持下去  hold on
+ * 记录员：Francis
+ * 日期：2019年4月24日
+ **/
+// 665. 非递减数列 2019年4月24日
+fun checkPossibility(nums: IntArray): Boolean {
+    return false
+}
+
+fun main(args: Array<String>) {
+    println(checkPossibility(intArrayOf()))
+
+}
