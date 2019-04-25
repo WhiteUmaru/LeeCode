f0e710548b9b07a36cca75fe245d04f9401bcb52
root <1025567884@qq.com>
2019-04-25 11:16:53 +0800

    add problems 29

---
+++ src/29/Answer.kt
@@ -0,0 +1,17 @@
+
+/**
+ * 一个思维锻炼的项目
+ * 请坚持下去  hold on
+ * 记录员：Francis
+ * 日期：2019年4月25日
+ **/
+// 29. 两数相除 2019年4月25日
+fun divide(dividend: Int, divisor: Int): Int {
+
+    return 0
+}
+
+fun main(args: Array<String>) {
+    println(divide(10, 3))
+
+}
